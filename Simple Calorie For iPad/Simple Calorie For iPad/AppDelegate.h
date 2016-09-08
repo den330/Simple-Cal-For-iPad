3e697a62bcd02d3b8ffb548bd8d46b2b38a22bac
JackY <wo131485508@qq.com>
2016-09-08 08:50:23 -0400

    First Commit

---
+++ Simple Calorie For iPad/Simple Calorie For iPad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Simple Calorie For iPad
+//
+//  Created by Yaxin Yuan on 16/9/8.
+//  Copyright © 2016年 Yaxin Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Simple Calorie For iPad/Simple Calorie For iPad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Simple Calorie For iPad
+//
+//  Created by Yaxin Yuan on 16/9/8.
+//  Copyright © 2016年 Yaxin Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
